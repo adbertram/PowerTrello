38f93dfee2125d0e9e528a1a3834b04e9db5fef5
Adam Bertram <adbertram@gmail.com>
2015-12-08 20:49:44 -0600

    mucho new functions

--- Trello.psm1
+++ Trello.psm1
@@ -249,6 +249,10 @@ function Get-TrelloCard
 		[ValidateNotNullOrEmpty()]
 		[object]$Board,
 		
+		[Parameter(ParameterSetName = 'Name')]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name,
+		
 		[Parameter(ParameterSetName = 'Label')]
 		[ValidateNotNullOrEmpty()]
 		[string]$Label,
@@ -274,9 +278,13 @@ function Get-TrelloCard
 			{
 				$cards
 			}
+			elseif ($PSBoundParameters.ContainsKey('Name'))
+			{
+				$cards | where {$_.Name -eq $Name}
+			}
 			else
 			{
-				$cards	
+				$cards
 			}
 		}
 		catch
@@ -303,6 +311,278 @@ function Get-TrelloCardLabel
 		{
 			$uri = "$baseUrl/boards/{0}/labels?{1}" -f $BoardId, $trelloConfig.String
 			Invoke-RestMethod -Uri $uri
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Set-TrelloCardList
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Id')]
+		[string]$CardId,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$ListId
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+			$uri = "$baseUrl/cards/{0}?idList={1}&{2}" -f $CardId, $ListId, $trelloConfig.String
+			Invoke-RestMethod -Uri $uri -Method Put
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Add-TrelloCardMember
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory,ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Card,
+	
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$MemberId
+		
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+			if ($Card.idMembers)
+			{
+				throw 'Existing members found on card. This is not supported yet.'
+			}
+			else
+			{
+				$uri = "$baseUrl/cards/{0}?idMembers={1}&{2}" -f $Card.Id, $MemberId, $trelloConfig.String	
+			}
+			
+			Invoke-RestMethod -Uri $uri -Method Put
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Get-TrelloMember
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Id')]
+		[string]$BoardId
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+			Invoke-RestMethod -Uri ("$baseUrl/boards/{0}/members?{1}" -f $BoardId, $trelloConfig.String)
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Remove-TrelloCardMember
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Card,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$MemberId
+		
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			$uri = "$baseUrl/cards/{0}/idMembers/{1}?{2}" -f $Card.Id, $MemberId, $trelloConfig.String
+			Invoke-RestMethod -Uri $uri -Method Delete
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Get-Checklist
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Card,
+	
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name
+		
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			$checkLists = Invoke-RestMethod -Uri ("$baseUrl/cards/{0}/checklists?{1}" -f $Card.Id, $trelloConfig.String)
+			if ($PSBoundParameters.ContainsKey('Name')) {
+				$checkLists | where {$_.name -eq $Name}
+			}
+			else
+			{
+				$checkLists	
+			}
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Get-ChecklistItem
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[pscustomobject]$Checklist,
+	
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+			if ($PSBoundParameters.ContainsKey('Name')) {
+				$checklist.checkItems | where {$_.Name -eq $Name}
+			}
+			else
+			{
+				$checklist.checkItems
+			}
+			
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Disable-ChecklistItem
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Card,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Checklist,
+		
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[pscustomobject]$ChecklistItem
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+			$params = @{
+				'Uri' = "$baseUrl/cards/{0}/checklist/{1}/checkItem/{2}?state=false&{3}" -f $Card.Id, $Checklist.Id, $ChecklistItem.Id, $trelloConfig.String
+				'Method' = 'Put'
+			}
+			Invoke-RestMethod @params
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function Enable-ChecklistItem
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Card,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Checklist,
+		
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[pscustomobject]$ChecklistItem
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try
+		{
+			$params = @{
+				'Uri' = "$baseUrl/cards/{0}/checklist/{1}/checkItem/{2}?state=true&{3}" -f $Card.Id, $Checklist.Id, $ChecklistItem.Id, $trelloConfig.String
+				'Method' = 'Put'	
+			}
+			Invoke-RestMethod @params
 		}
 		catch
 		{
