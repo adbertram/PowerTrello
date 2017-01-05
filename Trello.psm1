dc3d528e501d95d0a3c2588cc2748d23d5a8af94
Ryan Ephgrave <Ryan2065@gmail.com>
2017-01-05 08:49:10 -0600

    Added New-TrelloBoardList and New-TrelloListCard

--- Trello.psm1
+++ Trello.psm1
@@ -672,3 +672,165 @@ function Get-TrelloCardAttachment
 		}
 	}
 }
+
+function New-TrelloBoardList
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Id')]
+		[string]$BoardId,
+		
+		[Parameter(Mandatory)]
+		[ValidateNotNullOrEmpty()]
+		[string]$Name,
+
+        [Parameter()]
+		[string]$idListSource,
+
+        [Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[string]$position = 'top'
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+            $NewListHash = @{
+                'name'=$Name
+                'idBoard' = $BoardId
+                'pos' = $position
+            }
+            if(-not [string]::IsNullOrEmpty($idListSource))
+            {
+                $NewListHash['idListSource'] = $idListSource
+            }
+
+            $RestParams = @{
+                'uri' = "$baseUrl/lists?$($trelloConfig.String)"
+                'Method' = 'Post'
+                'Body' = $NewListHash
+            }
+
+			Invoke-RestMethod @RestParams
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
+function New-TrelloListCard
+{
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory,ValueFromPipelineByPropertyName)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Id')]
+		[string]$ListId,
+		
+		[Parameter()]
+		[string]$Name,
+
+        [Parameter()]
+		[string]$Description,
+
+        [Parameter()]
+		[string]$Position = 'bottom',
+
+        [Parameter()]
+		[string]$idMembers,
+
+        [Parameter()]
+		[string]$idLabels,
+
+        [Parameter()]
+		[string]$urlSource,
+
+        [Parameter()]
+		[string]$fileSource,
+
+        [Parameter()]
+		[string]$idCardSource,
+
+        [Parameter()]
+		[string]$keepFromSource
+	)
+	begin
+	{
+		$ErrorActionPreference = 'Stop'
+	}
+	process
+	{
+		try
+		{
+            $NewCardHash = @{
+                'idList'=$ListId
+            }
+            if(-not [string]::IsNullOrEmpty($Name))
+            {
+                $NewCardHash['name'] = $Name
+            }
+
+            if(-not [string]::IsNullOrEmpty($Description))
+            {
+                $NewCardHash['desc'] = $Description
+            }
+
+            if(-not [string]::IsNullOrEmpty($Position))
+            {
+                $NewCardHash['pos'] = $Position
+            }
+
+            if(-not [string]::IsNullOrEmpty($idMembers))
+            {
+                $NewCardHash['idMembers'] = $idMembers
+            }
+
+            if(-not [string]::IsNullOrEmpty($idLabels))
+            {
+                $NewCardHash['idLabels'] = $idLabels
+            }
+
+            if(-not [string]::IsNullOrEmpty($urlSource))
+            {
+                $NewCardHash['urlSource'] = $urlSource
+            }
+
+            if(-not [string]::IsNullOrEmpty($fileSource))
+            {
+                $NewCardHash['fileSource'] = $fileSource
+            }
+
+            if(-not [string]::IsNullOrEmpty($idCardSource))
+            {
+                $NewCardHash['idCardSource'] = $idCardSource
+            }
+
+            if(-not [string]::IsNullOrEmpty($keepFromSource))
+            {
+                $NewCardHash['keepFromSource'] = $keepFromSource
+            }
+
+            $RestParams = @{
+                'uri' = "$baseUrl/cards?$($trelloConfig.String)"
+                'Method' = 'Post'
+                'Body' = $NewCardHash
+            }
+
+			Invoke-RestMethod @RestParams
+		}
+		catch
+		{
+			Write-Error $_.Exception.Message
+		}
+	}
+}
