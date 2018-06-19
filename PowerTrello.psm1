d9fefecc8329efab577d88a9373d0115d6cff66f
Adam Bertram <adbertram@gmail.com>
2018-06-19 12:25:43 -0500

    Added Get-TrelloCustomField

--- PowerTrello.psm1
+++ PowerTrello.psm1
@@ -401,6 +401,27 @@ function Get-TrelloMember {
 	}
 }
 
+function Get-TrelloCustomField {
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+		[ValidateNotNullOrEmpty()]
+		[Alias('Id')]
+		[string]$BoardId
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try {
+			Invoke-RestMethod -Uri ("$baseUrl/boards/{0}/customFields?{1}" -f $BoardId, $trelloConfig.String)
+		} catch {
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
 function Remove-TrelloCardMember {
 	[CmdletBinding()]
 	param
