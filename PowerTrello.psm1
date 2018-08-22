b8dd9b13828d882626a52aeebfeb60e678c1b0f5
Adam Bertram <adbertram@gmail.com>
2018-08-22 11:54:04 -0500

    Fix to Set-CustomItem that prevented updating list items

--- PowerTrello.psm1
+++ PowerTrello.psm1
@@ -726,21 +726,30 @@ function Set-CustomField {
 
 	$ErrorActionPreference = 'Stop'
 
+	$RestParams = @{
+		Method  = 'PUT'
+		ContentType = 'application/json'
+	}
+
 	if (-not ($cusField = (Get-TrelloCustomField -BoardId $Card.idBoard) | where {$_.name -eq $CustomFieldName})) {
 		Write-Error -Message "Custom field [$($CustomFieldName)] could not be found on the board."
 	} else {
 		if ('options' -in $cusField.PSObject.Properties.Name) {
 			$cusFieldId = ($cusField.options | where { $_.Value.text -eq $CustomFieldValue }).id
-			$uri = '{0}/card/{1}/customField/{2}/item?idValue={3}&{4}' -f $baseUrl, $Card.Id, $cusField.id, $cusFieldId, $trelloConfig.String
+			$uri = '{0}/card/{1}/customField/{2}/item?{3}' -f $baseUrl, $Card.Id, $cusField.Id, $trelloConfig.String
+			$body = (ConvertTo-Json @{ 
+				'idValue' = $cusFieldid
+			})
 		} else {
 			$uri = '{0}/card/{1}/customField/{2}/item?{3}' -f $baseUrl, $Card.Id, $cusField.id, $trelloConfig.String
+			$body = (ConvertTo-Json @{ 'value' = @{ $cusField.type = $CustomFieldValue }})
 		}
 
 		$RestParams = @{
 			Uri     = $uri
 			Method  = 'PUT'
-			Body = (ConvertTo-Json @{ 'value' = @{ $cusField.type = $CustomFieldValue }})
 			ContentType = 'application/json'
+			Body = $body
 		}
 
 		$null = Invoke-RestMethod @RestParams
