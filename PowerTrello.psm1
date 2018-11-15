97d66d37de3faab11d598e15055308d403b702a8
Adam Bertram <adbertram@gmail.com>
2018-11-15 16:06:43 -0600

    Added Get-TrelloBoardAction

--- PowerTrello.psm1
+++ PowerTrello.psm1
@@ -1113,6 +1113,48 @@ function Get-TrelloCardAction {
 	}
 }
 
+function Get-TrelloBoardAction {
+	[CmdletBinding()]
+	param
+	(
+		[Parameter(Mandatory, ValueFromPipeline)]
+		[ValidateNotNullOrEmpty()]
+		[object]$Board,
+	
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('updateCard')] ## More are possible but haven't been tested
+		[string]$ActionFilter,
+
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[ValidateSet('idList')]
+		[string]$ActionFilterValue, ## More exist but haven't been tested,
+
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+		[datetime]$Since
+	)
+	begin {
+		$ErrorActionPreference = 'Stop'
+	}
+	process {
+		try {
+			if ($PSBoundParameters.ContainsKey('ActionFilter')) {
+				$uri = "$baseUrl/boards/{0}/actions?filter={1}:{2}&filter=all&limit=1000&{3}" -f $Board.Id, $ActionFilter, $ActionFilterValue, $trelloConfig.String
+			} elseif ($PSBoundParameters.ContainsKey('Since')) {
+				$utcTime = $Since.ToUniversalTime().ToString('o')
+				$uri = "$baseUrl/boards/{0}/actions?since={1}&filter=all&limit=1000&{2}" -f $Board.Id, $utcTime, $trelloConfig.String
+			} else {
+				$uri = "$baseUrl/boards/{0}/actions?filter=all&limit=1000&{1}" -f $Board.Id, $trelloConfig.String
+			}
+			Invoke-RestMethod -Uri $uri
+		} catch {
+			Write-Error $_.Exception.Message
+		}
+	}
+}
+
 function Set-TrelloCustomField {
 	[CmdletBinding()]
 	param
