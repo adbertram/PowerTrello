78aee34e389ad3a173ae5d3a2e986b2b9b0e022b
Adam Bertram <adam@adams-mbp.lan>
2019-12-16 14:03:00 -0600

    Get-TrelloCustomField expands JSON output

--- Public/Get-TrelloCustomField.ps1
+++ Public/Get-TrelloCustomField.ps1
@@ -1,20 +1,22 @@
 function Get-TrelloCustomField {
-	[CmdletBinding()]
-	param
-	(
-		[Parameter(Mandatory, ValueFromPipelineByPropertyName)]
-		[ValidateNotNullOrEmpty()]
-		[Alias('Id')]
-		[string]$BoardId
-	)
-	begin {
-		$ErrorActionPreference = 'Stop'
-	}
-	process {
-		try {
-			Invoke-RestMethod -Uri ("$script:baseUrl/boards/{0}/customFields?{1}" -f $BoardId, $trelloConfig.String)
-		} catch {
-			Write-Error $_.Exception.Message
-		}
-	}
+    [CmdletBinding()]
+    param
+    (
+        [Parameter(Mandatory, ValueFromPipelineByPropertyName)]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Id')]
+        [string]$BoardId
+    )
+    begin {
+        $ErrorActionPreference = 'Stop'
+    }
+    process {
+        try {
+            foreach ($field in (Invoke-RestMethod -Uri ("$script:baseUrl/boards/{0}/customFields?{1}" -f $BoardId, $trelloConfig.String))) {
+                $field
+            }
+        } catch {
+            Write-Error $_.Exception.Message
+        }
+    }
 }
